39dae8c801bfa2add6c4b75f5403e2a9ed2f037f
Ubaid-ur-rehman Muhammad <zdac091@live.rhul.ac.uk>
2020-11-05 16:36:47 +0000

    Orders class

--- orders.py
+++ orders.py
@@ -7,11 +7,18 @@ class Orders(Menu):
         self.order_list = []
 
     def taking_order(self):
-        print("Hi, What will you be having today?")
+        print("Hi,")
+        super().main_course_display()
+        print("What will you be having today?")
         i = 1
         while i < 4:
-            self.order_list.append(input(f"Order {i}: "))
-            i += 1
+            order = input(f"Order {i}: ")
+            if order in Menu().main_course:
+                self.order_list.append(order)
+                i += 1
+            else:
+                print("please choose an option from the menu")
+                super().main_course_display()
 
 # x = Orders()
 # x.taking_order()
